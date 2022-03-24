766f29fad13e0fcf056aa63326f0ba1b8bec2e79
Nir Gazit <nir.gzt@gmail.com>
2022-03-24 11:23:14 +0200

    Implemented Twitter Proxy

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const express = require("express");
+import express, { Router } from "express";
+import { join } from "path";
+import serverless from "serverless-http";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const router = Router();
+router.get("/tweet/:id", async (req, res) => {
+  const twitterRes = await fetch(
+    `https://api.twitter.com/2/tweets?ids=${req.params.id}&tweet.fields=text`,
+    {
+      headers: { Authorization: `Bearer ${process.env.TWITTER_AUTH_TOKEN}` },
+    }
+  );
+  const twitterResJson = await twitterRes.json();
+  res.json({ data: twitterResJson.data[0].text });
+});
 
-app.get("/", (req, res) => res.send("TEST"));
+app.use("/.netlify/functions/server", router); // path must route to lambda
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+export default app;
+export const handler = serverless(app);
