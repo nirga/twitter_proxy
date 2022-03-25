32c2b80ad4366b6eda42c69b69ea944772402c0b
Nir Gazit <nir.gzt@gmail.com>
2022-03-25 16:39:11 +0300

    Added username validation

--- functions/app.js
+++ functions/app.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 const handler = async (event, context) => {
   const twitterRes = await fetch(
-    `https://api.twitter.com/2/tweets?ids=${event.queryStringParameters.tweet}&tweet.fields=text`,
+    `https://api.twitter.com/2/tweets?ids=${event.queryStringParameters.tweet}&tweet.fields=text&expansions=author_id&user.fields=username`,
     {
       headers: { Authorization: `Bearer ${process.env.TWITTER_AUTH_TOKEN}` },
     }
@@ -11,13 +11,14 @@ const handler = async (event, context) => {
 
   console.log();
 
+  const isValid =
+    event.queryStringParameters.username ===
+      twitterResJson.includes.users[0].username &&
+    twitterResJson.data[0].text.includes(event.queryStringParameters.challenge);
+
   return {
     statusCode: 200,
-    body: twitterResJson.data[0].text.includes(
-      event.queryStringParameters.challenge
-    )
-      ? JSON.stringify({ data: "1" })
-      : JSON.stringify({ data: "1" }),
+    body: JSON.stringify({ data: isValid ? "1" : "0" }),
   };
 };
 
