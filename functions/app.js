3cfec17a861743a23b5b22aefd9c875b8d0f1a0e
Nir Gazit <nir.gzt@gmail.com>
2022-03-26 17:25:22 +0300

    Added logging

--- functions/app.js
+++ functions/app.js
@@ -1,6 +1,10 @@
 import fetch from "node-fetch";
 
 const handler = async (event, context) => {
+  console.log(
+    `Request: Checking Tweet ID ${event.queryStringParameters.tweet}`
+  );
+
   const twitterRes = await fetch(
     `https://api.twitter.com/2/tweets?ids=${event.queryStringParameters.tweet}&tweet.fields=text&expansions=author_id&user.fields=username`,
     {
@@ -9,12 +13,16 @@ const handler = async (event, context) => {
   );
   const twitterResJson = await twitterRes.json();
 
-  console.log();
+  console.log(
+    `Got Tweet by ${twitterResJson.includes.users[0].username}: ${twitterResJson.data[0].text}`
+  );
 
   const isValid =
     event.queryStringParameters.username ===
       twitterResJson.includes.users[0].username &&
     twitterResJson.data[0].text.includes(event.queryStringParameters.challenge);
+
+  console.log(`isValid=${isValid}`);
 
   return {
     statusCode: 200,
